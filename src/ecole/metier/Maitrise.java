1d6445ed98345ecab7c68d8ee3f1953795323700
Arnaud L.Celestino <arnaud.1985@hotmail.com>
2023-02-28 13:51:57 +0100

    Maj
    Maj du code, ajout de la documentation

--- src/ecole/metier/Maitrise.java
+++ src/ecole/metier/Maitrise.java
@@ -1,6 +1,101 @@
 package ecole.metier;
 
+import java.util.Objects;
+
+/**
+ * Classe Métier de la maitrise
+ * @author Arnaud
+ * @version 1.0
+ * @see Formateur
+ * @see Cours
+ */
 public class Maitrise {
+    /**
+     * Identifiant unique de la maitrise
+     */
+    protected int idMaitrise;
 
+    /**
+     * Degrès de maitrise
+     */
+    protected int degreMaitrise;
 
+    /**
+     * Cours relié à la maitrise
+     */
+    protected Cours cours;
+
+    /**
+     * Formateur relié à la maitise
+     */
+    protected Formateur formateur;
+
+    /**
+     * Constructeur par défaut
+     */
+    public Maitrise(){}
+
+    /**
+     * Constructeur Paramétré
+     * @param idMaitrise identifiant de la maitrise
+     * @param degreMaitrise Degré de la maitrise 1 2 ou 3
+     */
+    public Maitrise(int idMaitrise, int degreMaitrise, Cours cours, Formateur formateur) {
+        this.idMaitrise = idMaitrise;
+        this.degreMaitrise = degreMaitrise;
+        this.cours = cours;
+        this.formateur = formateur;
+    }
+
+    public int getIdMaitrise() {
+        return idMaitrise;
+    }
+
+    public void setIdMaitrise(int idMaitrise) {
+        this.idMaitrise = idMaitrise;
+    }
+
+    public int getDegreMaitrise() {
+        return degreMaitrise;
+    }
+
+    public void setDegreMaitrise(int degreMaitrise) {
+        this.degreMaitrise = degreMaitrise;
+    }
+
+    /**
+     * test d'égalité base sur le degré de maitrise
+     * @param o autre degré de maitrise
+     * @return égalité ou pas
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Maitrise maitrise)) return false;
+        return degreMaitrise == maitrise.degreMaitrise;
+    }
+
+    /**
+     * calcul du hashcode basé sur le degreMaitrise
+     * @return hashcode du degré de maitrise
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(degreMaitrise);
+    }
+
+    /**
+     * méthode toString
+     *
+     * @return informations complètes de la maitrise
+     */
+    @Override
+    public String toString() {
+        return "Maitrise{" +
+                "idMaitrise=" + idMaitrise +
+                ", degreMaitrise=" + degreMaitrise +
+                ", cours=" + cours +
+                ", formateur=" + formateur +
+                '}';
+    }
 }
--- src/ecole/metier/SessionCours.java
+++ src/ecole/metier/SessionCours.java
@@ -1,5 +1,184 @@
 package ecole.metier;
 
-public class SessionCours {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
 
+public class SessionCours {
+    /**
+     * Identifiant de la session de cours
+     */
+    protected int idSessionCours;
+
+    /**
+     * Date de début de la sesison de cours
+     */
+    protected Date dateDebut;
+
+    /**
+     * Date de fin de la session de cours
+     */
+    protected Date dateFin;
+
+    /**
+     * Nombre d'inscrits pour la session de cours
+     */
+    protected int nbreInscrits;
+
+    /**
+     * Liste des formateurs prévus pour la session de cours
+     */
+    protected ArrayList<Formateur> listFormateurs = new ArrayList();
+
+    /**
+     * Liste des cours qui sont prévus pour la session de cours
+     */
+    protected ArrayList<Cours> listeCours = new ArrayList<>();
+
+
+    /**
+     * getter idSessionCours
+     *
+     * @return identifiant de la session de cours
+     */
+    public int getIdSessionCours() {
+        return idSessionCours;
+    }
+
+    /**
+     * setter idSessionCours
+     *
+     * @param idSessionCours identifiant de la session de cours
+     */
+    public void setIdSessionCours(int idSessionCours) {
+        this.idSessionCours = idSessionCours;
+    }
+
+    /**
+     * getter dateDebut
+     *
+     * @return date de début de la session
+     */
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    /**
+     * setter dateDebut
+     *
+     * @param dateDebut date de début de la session
+     */
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    /**
+     * getter dateFin
+     *
+     * @return date de fin de la session
+     */
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    /**
+     * setter dateFin
+     *
+     * @param dateFin date de fin de la session
+     */
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    /**
+     * getter nbreInscrits
+     *
+     * @return identifiant du client
+     */
+    public int getNbreInscrits() {
+        return nbreInscrits;
+    }
+
+    /**
+     * setter nbreInscrits
+     *
+     * @param nbreInscrits nombre d'inscrits dans la session de cours
+     */
+    public void setNbreInscrits(int nbreInscrits) {
+        this.nbreInscrits = nbreInscrits;
+    }
+
+    /**
+     * getter listFormateurs
+     *
+     * @return la liste des formateurs
+     */
+    public ArrayList<Formateur> getListFormateurs() {
+        return listFormateurs;
+    }
+
+    /**
+     * setter listeFormateurs
+     *
+     * @param listFormateurs liste des formateurs
+     */
+    public void setListFormateurs(ArrayList<Formateur> listFormateurs) {
+        this.listFormateurs = listFormateurs;
+    }
+
+    /**
+     * getter listeCours
+
+     * @return la liste des cours
+     */
+    public ArrayList<Cours> getListeCours() {
+        return listeCours;
+    }
+
+    /**
+     * setter listeCours
+     *
+     * @param listeCours liste des cours
+     */
+    public void setListeCours(ArrayList<Cours> listeCours) {
+        this.listeCours = listeCours;
+    }
+
+    /**
+     * égalité de deux sessions  basées sur le triplet nombre d'inscrits, date de début et date de fin
+     * @param o autre élément
+     * @return égalité ou pas
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SessionCours that)) return false;
+        return nbreInscrits == that.nbreInscrits && Objects.equals(dateDebut, that.dateDebut) && Objects.equals(dateFin, that.dateFin);
+    }
+
+    /**
+     * calcul du hashcode de la session basé sur le duo date de début et date de fin de la session
+     * @return valeur du hashcode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(dateDebut, dateFin);
+    }
+
+    /**
+     * méthode toString
+     *
+     * @return informations complètes de la session de cours
+     */
+    @Override
+    public String toString() {
+        return "SessionCours{" +
+                "idSessionCours=" + idSessionCours +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", nbreInscrits=" + nbreInscrits +
+                ", listFormateurs=" + listFormateurs +
+                ", listeCours=" + listeCours +
+                '}';
+    }
 }
